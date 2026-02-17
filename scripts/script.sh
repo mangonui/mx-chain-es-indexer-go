f66f3f7c8b779feba7d7f3a0e1ad1b9947ebe48d
miiu <mihai9625@gmail.com>
2026-02-17 17:00:26 +0200

    rollback es version

--- scripts/script.sh
+++ scripts/script.sh
@@ -20,7 +20,6 @@ start() {
    -e "discovery.type=single-node" -e "xpack.security.enabled=false" -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
     docker.elastic.co/elasticsearch/elasticsearch:${ES_VERSION}
   # Wait elastic cluster to start
-  docker ps -a
   echo "Waiting Elasticsearch cluster to start..."
   sleep 30s
   docker ps -a
